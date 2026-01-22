204b752be814b07144ad60fd3a7eb671e5be9b05
abir2776 <osmangoni255@gmail.com>
2026-01-22 12:26:38 +0600

    added access token and refresh token after register

--- core/rest/views/register.py
+++ core/rest/views/register.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from ..serializers import register
 
@@ -14,7 +16,30 @@ class PublicUserRegistration(APIView):
         serializer = register.PublicUserRegistrationSerializer(
             data=request.data, context={"request": request}
         )
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(True, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            email = request.data.get("email")
+            password = request.data.get("password")
+            user = authenticate(request, email=email, password=password)
+
+            if not user:
+                return Response(
+                    {"detail": "Authentication failed after registration"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            refresh = RefreshToken.for_user(user)
+
+            return Response(
+                {
+                    "access": str(refresh.access_token),
+                    "refresh": str(refresh),
+                    "user": {
+                        "id": user.id,
+                        "email": user.email,
+                    },
+                },
+                status=status.HTTP_201_CREATED,
+            )
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
