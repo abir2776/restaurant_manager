9343172b80668ee7cad1ffe43e812bf181bd5e77
abir2776 <osmangoni255@gmail.com>
2026-02-02 10:27:53 +0600

    Added some changes in product model

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -31,11 +31,13 @@ class Image(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ManyToManyField(Category)
-    images = models.ManyToManyField(Image, related_name="products")
+    category = models.ManyToManyField(Category, null=True, blank=True)
+    images = models.ManyToManyField(
+        Image, related_name="products", null=True, blank=True
+    )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=500)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
     is_popular = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
