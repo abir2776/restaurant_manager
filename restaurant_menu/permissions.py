53ab29bd27de6ba2f98ed2dfb76883281f3dfbf7
abir2776 <osmangoni255@gmail.com>
2026-02-19 12:57:23 +0600

    updated permission

--- restaurant_menu/permissions.py
+++ restaurant_menu/permissions.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import BasePermission
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         user = request.user
-        return user and user.is_authenticated and getattr(user, "role", None) == "ADMIN"
+        return user and user.is_authenticated and getattr(user, "role", None) in ["ADMIN","SUPER_ADMIN"]
 
 
 class IsSuperUser(BasePermission):
