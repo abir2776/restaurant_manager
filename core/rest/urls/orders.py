cf9b576876e4965663057d1c18eb44228d64ca0e
abir2776 <osmangoni255@gmail.com>
2026-02-22 16:14:42 +0600

    fixed error

--- core/rest/urls/orders.py
+++ core/rest/urls/orders.py
@@ -4,5 +4,5 @@ from core.rest.views.orders import AdminOrderDetailsView, AdminOrderListView
 
 urlpatterns = [
     path("", AdminOrderListView.as_view(), name="admin-order-list"),
-    path("<int:pk>", AdminOrderDetailsView.as_view(), name="admin-order-details"),
+    path("<int:id>", AdminOrderDetailsView.as_view(), name="admin-order-details"),
 ]
