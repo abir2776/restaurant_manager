55a0cde4c9f83aa5bf7240b68ea559e1bbb4a976
abir2776 <osmangoni255@gmail.com>
2026-02-23 08:34:21 +0600

    added search and filter in admin order list

--- core/rest/views/orders.py
+++ core/rest/views/orders.py
@@ -3,12 +3,21 @@ from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
 from core.rest.serializers.orders import AdminOrderSerializer
 from order.models import Order
 from restaurant_menu.permissions import IsAdmin
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class AdminOrderListView(ListAPIView):
     serializer_class = AdminOrderSerializer
     permission_classes = [IsAdmin]
     queryset = Order.objects.filter()
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    search_fields = ["id", "payment_set__id", "user__first_name", "user__last_name"]
+    filterset_fields = ["status"]
 
 
 class AdminOrderDetailsView(RetrieveUpdateAPIView):
